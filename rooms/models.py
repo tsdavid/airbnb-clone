0a609d9c28333bf13a72067f9b0e33e2d8bc5bb4
tsdavid <ipackcorpdavid@gmail.com>
2019-09-28 16:30:12 +0900

    #4.5 Meta Class and Photos Model

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as users_models
 
 # pack import order
 #  1. python pack 2. django pack 3. third party pack 4. my pack
@@ -24,28 +23,46 @@ class RoomType(AbstractItem):
 
     """ RoomType Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Room Type"
 
 
 class Amenity(AbstractItem):
 
-    """ RoomType Model Definition """
+    """ Amenity Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Mode Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ House Rule Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey(
+        "Room", on_delete=models.CASCADE
+    )  # 다른 모델 가지고올때 string으로 해도 가지고 와도됨
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -65,13 +82,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(
-        users_models.User, on_delete=models.CASCADE
-    )  # host와 user 묶기
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rule = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)  # host와 user 묶기
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
