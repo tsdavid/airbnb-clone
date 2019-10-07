0a37100fe4ff4285de1103af3a2f0ee2ec04cf39
tsdavid <ipackcorpdavid@gmail.com>
2019-10-07 18:58:35 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -5,9 +5,9 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)  # paginator 생성
-    rooms = paginator.get_page(page)  # page를 넣어서 paginator
+    paginator = Paginator(room_list, 10, orphans=5)  # paginator 생성, orphans는 마지막 페이지에 기준 10하고 남은 나머지를 넣는 것
+    rooms = paginator.page(int(page))  # page를 넣어서 paginator
     print(vars(rooms.paginator))
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    return render(request, "rooms/home.html", {"page": rooms})
