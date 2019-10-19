6ef3414c203b6cbc809c7bdef76da7e8b1a0c652
tsdavid <ipackcorpdavid@gmail.com>
2019-10-19 15:10:30 +0900

    #13.6 Filtering Like a Boss part One

--- rooms/views.py
+++ rooms/views.py
@@ -69,5 +69,15 @@ def search(request):
 
     }
 
-    return render(request, "rooms/search.html", {**form, **choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+    filter_args["country"] = country
+    
+    if room_type != 0:
+        filter_args["room_type__pk__exact"] = room_type
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
     # ** 하면 unpack 다 풀어놓는 개념인듯
