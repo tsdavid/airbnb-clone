b859123e8b4de652bde60ce4ca052455b0df9295
tsdavid <ipackcorpdavid@gmail.com>
2019-10-22 16:28:06 +0900

    13.6 Filtering Like a Boss part Two

--- rooms/views.py
+++ rooms/views.py
@@ -25,8 +25,10 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     # request get 한 부분이 blank일 때 ANywhere로 돌림.
+    # if city is Anywhere dont filter by city
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
+    # if country is KR dont filter by country
     room_type = int(request.GET.get("room_type", 0))
 
     price = int(request.GET.get("price", 0))
@@ -35,8 +37,9 @@ def search(request):
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
 
-    instant = request.GET.get("instant", False)
-    superhost = request.GET.get("superhost", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
+
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -53,7 +56,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "superhost": superhost,
+        "superhost": superhost
     }
 
     room_types = models.RoomType.objects.all()
@@ -74,10 +77,40 @@ def search(request):
     if city != "Anywhere":
         filter_args["city__startswith"] = city
     filter_args["country"] = country
-    
+
     if room_type != 0:
         filter_args["room_type__pk__exact"] = room_type
-    rooms = models.Room.objects.filter(**filter_args)
 
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guest != 0:
+        filter_args["guest__gte"] = guest
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+    
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+    # print(s_amenities) => ["1", "2", "3"]
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
+            
+    rooms = models.Room.objects.filter(**filter_args)
     return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
     # ** 하면 unpack 다 풀어놓는 개념인듯
