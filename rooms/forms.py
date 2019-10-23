2a5f86c12ded214cb19685b0afd2a9ed5b04e184
tsdavid <ipackcorpdavid@gmail.com>
2019-10-23 11:03:47 +0900

    #13.9 Error Occur check it

--- rooms/forms.py
+++ rooms/forms.py
@@ -9,7 +9,7 @@ class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="KR").formfield()
     room_type = forms.ModelChoiceField(
-        empty_label="Any Kind", 
+        empty_label="Any Kind",
         required=False,
         queryset=models.RoomType.objects.all()
         )
@@ -20,5 +20,13 @@ class SearchForm(forms.Form):
     baths = forms.IntegerField(required=False)
     instant_book = forms.BooleanField(required=False)
     superhost = forms.BooleanField(required=False)
-    amenities = forms.ModelMultipleChoiceField(queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple)
-    facilities = forms.ModelMultipleChoiceField(queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple)
+    amenities = forms.ModelMultipleChoiceField(
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+        )
+    facilities = forms.ModelMultipleChoiceField(
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+        )
--- rooms/views.py
+++ rooms/views.py
@@ -24,6 +24,68 @@ class RoomDetail(DetailView):
 
 def search(request):
 
-    form = forms.SearchForm
+    country = request.GET.get("country")
 
-    return render(request, "rooms/search.html", {"form": form})
+    if country:
+        form = forms.SearchForm(request.GET)
+
+        if form.is_valid():
+
+            city = form.cleaned_data.get("city")
+            country = form.cleaned_data.get("country")
+            room_type = form.cleaned_data.get("room_type")
+            price = form.cleaned_data.get("price")
+            guests = form.cleaned_data.get("guests")
+            bedrooms = form.cleaned_data.get("bedrooms")
+            beds = form.cleaned_data.get("beds")
+            baths = form.cleaned_data.get("baths")
+            instant_book = form.cleaned_data.get("instant_book")
+            superhost = form.cleaned_data.get("superhost")
+            amenities = form.cleaned_data.get("amenities")
+            facilities = form.cleaned_data.get("facilities")
+
+            filter_args = {}
+
+            if city != "Anywhere":
+                filter_args["city__startswith"] = city
+
+            filter_args["country"] = country
+
+            if room_type is not None:
+                filter_args["room_type"] = room_type
+
+            if price is not None:
+                filter_args["price__lte"] = price
+
+            if guests is not None:
+                filter_args["guest__gte"] = guests
+
+            if bedrooms is not None:
+                filter_args["bedrooms__gte"] = bedrooms
+
+            if beds is not None:
+                filter_args["beds__gte"] = beds
+
+            if baths is not None:
+                filter_args["baths__gte"] = baths
+
+            if instant_book is True:
+                filter_args["instant_book"] = True
+
+            if superhost is True:
+                filter_args["host__superhost"] = True
+
+            for amenity in amenities:
+                filter_args["amenities"] = amenity
+                # Amenities가 Queryset으로 왔으니까 pk로 맞출 필요가 없음
+
+            for facility in facilities:
+                filter_args["facilities"] = facility
+
+            rooms = models.Room.objects.filter(**filter_args)
+
+    else:
+
+        form = forms.SearchForm()
+
+    return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
