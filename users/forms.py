3463df178a5e69e861397f0200f60b30eb13a24b
tsdavid <ipackcorpdavid@gmail.com>
2019-11-05 21:32:19 +0900

    #14.2 Validating Email

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -8,3 +9,24 @@ class LoginForm(forms.Form):
     # password form은 password form field가 따로 없다.
     # 그래서 CharField로 하고 그럼 보호가 안돼니까 widget으로 
     # PasswordInput을 하면 숫자를 가려준다.
+
+    def clean_email(self):
+        """ data를 clean 하게 가지고 와야 db에 저장되있는 email 이랑 pwd를 비교할 텐데
+        form에 받은 정보를 clean하게 해야한다
+        clean하게 하려면 함수를 쓰는데
+        함수 명이 임의로 정하는게 아니라
+        clean_필드명
+        으로 설정해줘야한다.
+        email 필드를 clean하고 싶으면
+        clean_email 해야함
+        """
+        email = self.cleaned_data.get("email")      # get the data from user sent us
+        try:
+            models.User.objects.get(username=email)     # try to find one
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    
+    def clean_password(self):
+        return "lulqwewqe"
--- users/views.py
+++ users/views.py
@@ -5,10 +5,23 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "tsdavid@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            """ if there error on the form 
+            it didnt work
+            """
+            print(form.cleaned_data)    
+            """ 
+                일단 form에서 받은 data를 장고에게 보내서
+                사용허가를 받아야 
+                그제서야 우리는 clean data를 사용할 수 있다.
+
+                또한 form에서 clean_field를 하면
+                form에서 설정한 데이터로 return이 된다.
+            """
+        return render(request, "users/login.html", {"form": form})
     
