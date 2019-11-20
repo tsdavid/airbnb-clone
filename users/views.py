a80c877823f5ac557ea6b150188f112027c031db
tsdavid <ipackcorpdavid@gmail.com>
2019-11-20 20:18:10 +0900

    #15.0 Signup Form

--- users/views.py
+++ users/views.py
@@ -23,3 +23,15 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "David",
+        "last_name": "Kim",
+        "email": "davidkim@naver.com",
+        
+    }
