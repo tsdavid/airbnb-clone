1ea22d410664e118602c1f2b4ce21e38de71fbfd
tsdavid <ipackcorpdavid@gmail.com>
2019-11-20 19:29:59 +0900

    #14.5 Always an Easier Way

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
@@ -11,17 +12,14 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            """ if there error on the form 
-            it didnt work
-            """
-            print(form.cleaned_data)    
-            """ 
-                일단 form에서 받은 data를 장고에게 보내서
-                사용허가를 받아야 
-                그제서야 우리는 clean data를 사용할 수 있다.
-
-                또한 form에서 clean_field를 하면
-                form에서 설정한 데이터로 return이 된다.
-            """
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
-    
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
